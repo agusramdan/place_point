da2c64cf194d7cbaef35d0dfc2652d12b784e72d
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2022-10-20 11:46:34 +0700

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Place Point Module',
+    'version': '13.0.0',
+    'summary': 'Place Module',
+    'author': 'Agus Muhammad Ramdan',
+    'website': 'https://www.mycompany.com',
+    'depends': [ 
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
